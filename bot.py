182d57e3a74993873b648d7f1bf3405aab9526e1
ordinalcyber <tominoucorbac24@gmail.com>
2025-03-06 10:51:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ app = Flask("")
 def home() :
     return "le bot est en ligne"
 def run():
-    port = int(os.environ.get("PORT", 8080))
+    port = process.env.PORT || 4000;
     app.run(host='0.0.0.0',port=port)
 def keep_alive():
     t=Thread(target=run)
