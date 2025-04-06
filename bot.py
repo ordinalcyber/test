f7f6920a9d6293cf845e8259d69f8e971f60f77c
ordinalcyber <tominoucorbac24@gmail.com>
2025-04-06 20:52:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ exchange = ccxt.binance()
 SYMBOLS = "SOL/EUR"
 TIMEFRAME = "1m"
 WINDOW_OHLCV = 200  # Pour les prédictions dans analyze_market
-LIMIT_TEST = 250  # Pour l’entraînement et test total
+LIMIT_TEST = 200  # Pour l’entraînement et test total
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 Rotation = 0
 
